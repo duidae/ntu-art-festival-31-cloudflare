ed2b650d459d9cbeb479d02383a7d4ec0dd14f70
duidae <duidae@gmail.com>
2026-02-17 01:02:43 +0800

    fix: add map route

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
-import { TreasureHunt, NotFound } from "./scene";
+import { TreasureHunt, NotFound, MissionMap } from "./scene";
 import "./index.css";
 
 const TreasureHuntPaths = ['kzqpt9', 'mrvxa7'].map(siteId => `/treasure-hunt/${siteId}`);
@@ -12,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/map" element={<MissionMap setScene={() => {}} progress={{ m1: false, m2: false, m3: false, b1: false, b2: false, b3: false }} />} />
         {TreasureHuntPaths?.map((path) => <Route key={path} path={path} element={<TreasureHunt />} />)}
         <Route path="*" element={<NotFound />} />
       </Routes>

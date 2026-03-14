2e1d75ed86573028333497e87f5b832d0c5d2017
duidae <duidae@gmail.com>
2026-03-14 21:37:58 +0800

    fix: fix gps

--- src/react-app/constants/sideMissions.ts
+++ src/react-app/constants/sideMissions.ts
@@ -2,17 +2,17 @@ const treasureHunterDate = '20260314';
 export const TREASURE_HUNT_MISSIONS = [{
   title: `${treasureHunterDate} 尋寶任務1`,
   siteCode: 'kzqpt9',
-  coordinates: [25.017254, 121.540416],
+  coordinates: [25.0177262, 121.5359260],
   story: '/story/treasure-hunt-mission-1.json',
 }, {
   title: `${treasureHunterDate} 尋寶任務2`,
   siteCode: 'mrvxa7',
-  coordinates: [25.016325, 121.540081],
+  coordinates: [25.0173297, 121.5390890],
   story: '/story/treasure-hunt-mission-2.json',
 }, {
   title: `${treasureHunterDate} 尋寶任務3`,
   siteCode: 'gthui5',
-  coordinates: [25.0178591423562, 121.53998518789876],
+  coordinates: [25.0163250, 121.5400810],
   story: '/story/treasure-hunt-mission-3.json',
 }];
 export const ART_FESTIVAL_TREASURE_HUNT_PATHS = TREASURE_HUNT_MISSIONS.map(m => m.siteCode);
